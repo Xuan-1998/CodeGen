eceb2e2e578b077235d9bd3acee21a6e68c8bcdf
Yibo Zhao <yibozhao83@gmail.com>
2024-04-27 20:27:06 +0800

    Refine run_generated_code logic: Add online OJ choice (Slow, not recommended)

--- codeGen/utils/oj_interactions.py
+++ codeGen/utils/oj_interactions.py
@@ -2,73 +2,75 @@ import requests
 import logging
 import os
 import json
+import base64
 
-logger = logging.getLogger("__name__") 
+logger = logging.getLogger("__name__")
 
 BASE_URL = {
-    'local': "http://localhost:2358",
-    'online': "https://judge0-ce.p.rapidapi.com"
+    "local": "http://localhost:2358",
+    "online": "https://judge0-ce.p.rapidapi.com",
 }
 
 HEADERS = {
-    'Content-Type': 'application/json',
-    'X-RapidAPI-Key': os.environ.get('RAPIDAPI_KEY', ''),
-    'X-RapidAPI-Host': 'judge0-ce.p.rapidapi.com'
+    "Content-Type": "application/json",
+    "X-RapidAPI-Key": os.environ.get("RAPIDAPI_KEY", ""),
+    "X-RapidAPI-Host": "judge0-ce.p.rapidapi.com",
 }
 
-QUERY_PARAMS = {
-    'base64_encoded': 'false',
-    'fields': '*'
-}
+QUERY_PARAMS = {"base64_encoded": "false", "fields": "*"}
+
 
 # method: local or online
 # TODO: currently only supports online, investigating the problem with local server
-def submit_code(code: str, stdin: str, stdout: str, method = 'local') -> str:
-    language_id = {
-        'local': 71,
-        'online': 92
-    } # python
+def submit_code(code: str, stdin: str, stdout: str, method="local") -> str:
+    language_id = {"local": 71, "online": 92}  # python
 
     payload = {
         "language_id": language_id[method],
         "source_code": code,
         "stdin": stdin,
-        "expected_output": stdout
+        "expected_output": stdout,
     }
 
     url = f"{BASE_URL[method]}/submissions"
     logger.debug(f"Payload: {payload}")
 
     response = requests.post(url, json=payload, headers=HEADERS, params=QUERY_PARAMS)
-    token = response.json()['token']
+    token = response.json()["token"]
     logger.info(f"Token: {token}")
-    
+
     return token
 
-def submit_code_batch(code: str, test_cases: list[dict], method = 'local') -> list[str]:
-    language_id = {
-        'local': 71,
-        'online': 92
-    } # python
 
-    payload = {
-        "submissions":[]
-    }
+def submit_code_batch(code: str, test_cases: list[dict], method="local") -> list[str]:
+    language_id = {"local": 71, "online": 92}  # python
+
+    payload = {"submissions": []}
 
     for test_case in test_cases:
-        inputs = "\n".join(test_case['input']) if type(test_case['input']) is list else test_case['input']
+        inputs = (
+            "\n".join(test_case["input"])
+            if type(test_case["input"]) is list
+            else test_case["input"]
+        )
         if not inputs.endswith("\n"):
             inputs += "\n"
-        outputs = "\n".join(test_case['output']) if type(test_case['output']) is list else test_case['output']
+        outputs = (
+            "\n".join(test_case["output"])
+            if type(test_case["output"]) is list
+            else test_case["output"]
+        )
         if not outputs.endswith("\n"):
             outputs += "\n"
 
-        payload['submissions'].append({
-            "language_id": language_id[method],
-            "source_code": code,
-            "stdin": inputs,
-            "expected_output": outputs
-        })
+        payload["submissions"].append(
+            {
+                "language_id": language_id[method],
+                "source_code": code,
+                "stdin": inputs,
+                "expected_output": outputs,
+            }
+        )
 
     url = f"{BASE_URL[method]}/submissions/batch"
     logger.debug(f"Payload: {payload}")
@@ -77,10 +79,12 @@ def submit_code_batch(code: str, test_cases: list[dict], method = 'local') -> li
     try:
         response_data = response.json()
         if isinstance(response_data, list):
-            tokens = [item['token'] for item in response_data]
+            tokens = [item["token"] for item in response_data]
         else:
             if response.status_code != 200:
-                logger.error(f"HTTP Error: {response.status_code} with body {response.text}")
+                logger.error(
+                    f"HTTP Error: {response.status_code} with body {response.text}"
+                )
                 return []
             logger.error(f"Unexpected JSON structure: {response_data}")
             return []
@@ -88,19 +92,31 @@ def submit_code_batch(code: str, test_cases: list[dict], method = 'local') -> li
         logger.error(f"JSON decoding failed: {str(e)}")
         return []
 
-
     logger.info(f"Tokens: {tokens}")
 
     return tokens
 
-def get_submission(token: str, method = 'local'):
+
+def get_submission(token: str, method="local"):
     url = f"{BASE_URL[method]}/submissions/{token}"
     response = requests.get(url, params=QUERY_PARAMS, headers=HEADERS)
     if response.status_code != 200:
         logger.error(f"Error: {response.status_code}, {response.json()}")
     print(response.json())
-    logger.info(f"Response: Token: {response.json()['token']}, Description: {response.json()['status']['description']}")
+    logger.info(
+        f"Response: Token: {response.json()['token']}, Description: {response.json()['status']['description']}"
+    )
     return response.json()
+
+
+def get_status_and_output(response: str):
+    try:
+        decoded_bytes = base64.b64decode(response["stdout"])
+        decoded_str = decoded_bytes.decode('latin-1')
+    except:
+        decoded_str = ""
+    return response["status"]["description"], decoded_str
+
 
 def submit_officially(code: str, stdin: str, stdout: str) -> dict:
     # placeholder, will submit code to leetcode/codeforce, https://github.com/Sajantoor/LeetCode-API?tab=readme-ov-file
--- codeGen/utils/run_generated_code.py
+++ codeGen/utils/run_generated_code.py
@@ -1,10 +1,14 @@
-import os
+from oj_interactions import *
 import json
-import subprocess
+import os
 import pandas as pd
+import subprocess
+import time
+
 
 def calculate_accuracy(expected_output, actual_output):
     return expected_output.count(actual_output) / len(expected_output)
+
 
 def compare_outputs(py_file, json_file):
     with open(json_file) as f:
@@ -13,14 +17,23 @@ def compare_outputs(py_file, json_file):
     accuracy_rates = []
     expected_outputs = []
     actual_outputs = []
-    
+
     for test_case in test_cases:
         input_data = test_case["input"]
         expected_output = test_case["output"]
-        expected_output = [line.strip() for line in expected_output] if isinstance(expected_output, list) else expected_output.strip()
+        expected_output = (
+            [line.strip() for line in expected_output]
+            if isinstance(expected_output, list)
+            else expected_output.strip()
+        )
 
         # Run the Python file and capture the output
-        process = subprocess.Popen(["python", py_file], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        process = subprocess.Popen(
+            ["python", py_file],
+            stdin=subprocess.PIPE,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+        )
         try:
             actual_output, _ = process.communicate(input=input_data.encode(), timeout=1)
             actual_output = actual_output.decode().strip()
@@ -37,11 +50,37 @@ def compare_outputs(py_file, json_file):
     average_accuracy = sum(accuracy_rates) / len(accuracy_rates)
     return average_accuracy, expected_outputs, actual_outputs, accuracy_rates
 
+
+def get_acc_from_oj(py_file, json_file, method="online"):
+    with open(json_file) as f:
+        test_cases = json.load(f)
+
+    accuracy_rates = []
+    expected_outputs = []
+    actual_outputs = []
+
+    code = ""
+    with open(py_file, "r") as f:
+        code = f.read()
+    tokens = submit_code_batch(code, test_cases, method=method)
+    time.sleep(1)
+
+    for token in tokens:
+        result = get_submission(token, method=method)
+        status, output = get_status_and_output(result)
+
+        accuracy_rates.append(status)
+        expected_outputs.append(result["expected_output"])
+        actual_outputs.append(output)
+
+    average_accuracy = sum("Accepted" in item for item in accuracy_rates) / len(accuracy_rates)
+    return average_accuracy, expected_outputs, actual_outputs, accuracy_rates
+
+
 def run_generated_code(
-        root_dir: str = "results",
-        model: str = "subprocess" # "online" or "local" or 'subprocess'
+    root_dir: str = "results",
+    model: str = "subprocess",  # "online" or "local" or 'subprocess'
 ):
-    # root_dir = r"results"
     for method in os.listdir(root_dir):
         highest_accuracy = []
         if not os.path.isdir(os.path.join(root_dir, method)):
@@ -54,7 +93,10 @@ def run_generated_code(
             py_folder = os.path.join(root_dir, method, prob)
             py_files = [file for file in os.listdir(py_folder) if file.endswith(".py")]
 
-            if any(file.endswith(".csv") for file in os.listdir(py_folder)) or len(py_files) == 0:
+            if (
+                any(file.endswith(".csv") for file in os.listdir(py_folder))
+                or len(py_files) == 0
+            ):
                 continue
 
             average_accuracies = []
@@ -64,25 +106,39 @@ def run_generated_code(
 
             json_file = os.path.join(py_folder, "test.json")
             for py_file in py_files:
-                accuracy, expected_outputs, actual_outputs, accuracy_rates = compare_outputs(os.path.join(py_folder, py_file), json_file)
-                # print(f"{prob}{py_file}: Accuracy = {accuracy}")
+                if model == "subprocess":
+                    accuracy, expected_outputs, actual_outputs, accuracy_rates = (
+                        compare_outputs(os.path.join(py_folder, py_file), json_file)
+                    )
+                else:
+                    accuracy, expected_outputs, actual_outputs, accuracy_rates = (
+                        get_acc_from_oj(
+                            os.path.join(py_folder, py_file), json_file, method=model
+                        )
+                    )
                 average_accuracies.append(accuracy)
                 all_expected_outputs.append(expected_outputs)
                 all_actual_outputs.append(actual_outputs)
                 all_accuracy_rates.append(accuracy_rates)
 
-            df = pd.DataFrame({
-                "File": py_files,
-                "Average Accuracy": average_accuracies,
-                "Expected Output": all_expected_outputs,
-                "Actual Output": all_actual_outputs,
-                "Accuracy Rate": all_accuracy_rates
-            })
+            df = pd.DataFrame(
+                {
+                    "File": py_files,
+                    "Average Accuracy": average_accuracies,
+                    "Expected Output": all_expected_outputs,
+                    "Actual Output": all_actual_outputs,
+                    "Accuracy Rate": all_accuracy_rates,
+                }
+            )
             df.to_csv(f"{py_folder}/results.csv", index=False)
             highest_accuracy.append(max(average_accuracies))
-        
+
         if highest_accuracy:
-            print(f"{method}: Average Accuracy = {sum(highest_accuracy)}/{len(highest_accuracy)} = {sum(highest_accuracy)/len(highest_accuracy)}")
+            print(
+                f"{method}: Average Accuracy = {sum(highest_accuracy)}/{len(highest_accuracy)} = {sum(highest_accuracy)/len(highest_accuracy)}"
+            )
+
 
 if __name__ == "__main__":
+    # run_generated_code(model='online')
     run_generated_code()
