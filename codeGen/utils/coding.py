dd518a0feb9024b985e7ffdb13f177d318549674
Xuan Jiang <xuan@artemis.ist.berkeley.edu>
2024-06-05 21:14:21 +0000

    TODO: logic of keeping plans

--- codeGen/utils/coding.py
+++ codeGen/utils/coding.py
@@ -56,6 +56,11 @@ class Coding:
             if self.should_keep_plan(code):
                 pruned_plans.append(plan)
         return pruned_plans
+    
+    def should_keep_plan(self, code: str) -> bool:
+        # TODO: I can not find where you defined accuracy etc? I think we can just prune those with zero acc?
+        return "prune_condition" not in code
+    
     def zeroshot_coder(self) -> tuple[str, str]:
         message = [
             {
